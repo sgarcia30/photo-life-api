7f6512d14d22022da3f3c2478f1554dd56ce14dd
sgarcia30 <garciatres@gmail.com>
2018-04-12 11:44:08 -0400

    added CORS

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });

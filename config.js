437735c60fb8501aed9c6589839998d573c80bda
sgarcia30 <garciatres@gmail.com>
2018-04-12 12:24:28 -0400

    trying to setup CORS

---
+++ config.js
@@ -0,0 +1,9 @@
+'use strict'
+exports.DATABASE_URL ||
+	process.env.DATABASE_URL ||
+	global.DATABASE_URL;
+
+exports.PORT = process.env.PORT || 3000;
+exports.JWT_SECRET = process.env.JWT_SECRET || "photo-life";
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY  || "7d";
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "https://naughty-heisenberg-f90840.netlify.com/";
